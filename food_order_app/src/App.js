4861a7b20b1eecb17c26a11de1ed8189f277785c
KeshavBansal21 <keshav.bansal@unthinkable.co>
2021-11-28 11:04:49 +0530

    set default state of setCartIsShown to false

--- food_order_app/src/App.js
+++ food_order_app/src/App.js
@@ -4,7 +4,7 @@ import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart'
 
 function App() {
-  const [cartIsShown , setCartIsShown] = useState(true);
+  const [cartIsShown , setCartIsShown] = useState(false);
 
   const showCartHandler = ()=>{
     setCartIsShown(true);
