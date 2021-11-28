2dbe24e68668b525e738e9d3939f7611bdc9eb72
KeshavBansal21 <keshav.bansal@unthinkable.co>
2021-11-28 11:01:09 +0530

    shopping cart button working

--- food_order_app/src/components/Cart/Cart.js
+++ food_order_app/src/components/Cart/Cart.js
@@ -1,28 +1,38 @@
-import classes from './Cart.module.css';
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
 
-const Cart = (props) =>{
+const Cart = (props) => {
     const cartItems = (
-        <ul className="{classes['cart-item']}">
-            {[{id:'c1' , name:'Sushi' , amount:2, price:12.99}].map((item)=>{
-                <li>{item.name}</li>
-            })}
-        </ul>
-    )
+        <ul className={classes['cart-item']}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => {
 
-    return(
-        <div>
-            {cartItems}
-            <div className={classes.total}>
-                <span>Total Amount</span>
-                <span>35.62</span>
+          <li>{item.name}</li>
+      })}
+    </ul>
+  );
+      console.log("helhl")
+  return (
+    <Modal>
+        
+      {/* {cartItems} */}
+      <ul className={classes['cart-item']}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
 
-            </div>
-            <div className={classes.actions}>
-                <button className={classes['button--alt']}>Close</button>
-                <button className={classes.button}>Order</button>
-            </div>
+          <li>{item.name}</li>
+      ))}
+    </ul>
+      <div>
+        <div className={classes.total}>
+          <span>Total Amount</span>
+          <span>35.62</span>
         </div>
-    )
-}
+        <div className={classes.actions}>
+          <button className={classes["button--alt"]} onClick = {props.onClose}>Close</button>
+          <button className={classes.button}>Order</button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
 
 export default Cart;
